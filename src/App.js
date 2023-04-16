96b44ec17dfbe57f78aa4422174557f01bff2ba2
Bermick90 <120055844+Bermick90@users.noreply.github.com>
2023-04-16 23:54:55 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Select from './components/Select';
 import Input from './components/Input';
 import Result from './components/Result';
+import companyLogo from './assets/GRU.png';
 
 function App() {
   const [selectValue, setSelectValue] = useState('EUR');
@@ -40,12 +41,23 @@ function App() {
 
   return (
     <>
-      <form onSubmit={validateForm}>
-        <Input onChange={handleInputChange} />
-        <Select onChange={handleSelectChange} />
-        <input type="submit" value="Przelicz" />
-      </form>
-      <Result convertedValue={outputValue} />
+      <title>Pigeon Currency Calculator</title>
+      <div className="container">
+        <div className="logo">
+          <div className="app__logo">
+            <img src={companyLogo} alt="Gołąbek Gru" />
+          </div>
+          <h1 className="text--center">Pigeon Currency Calculator</h1>
+        </div>
+        <div className="calculation">
+          <form onSubmit={validateForm}>
+            <Input onChange={handleInputChange} />
+            <Select onChange={handleSelectChange} />
+            <input type="submit" value="Przelicz" />
+          </form>
+          <Result convertedValue={outputValue} />
+        </div>
+      </div>
     </>
   );
 }
